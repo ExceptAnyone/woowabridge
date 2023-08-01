87aceb5dc98ba825fd6074ca60d23d143a71f07f
장정안 <wjddks96@naver.com>
2023-08-01 12:31:14 +0900

    에러 메세지 추가

---
+++ src/Error.js
@@ -0,0 +1,7 @@
+const Error_Message = {
+    RANGE_OF_NUMBER : "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+    SELECT_ERROR : "[ERROR] 대문자 U 또는 대문자 D를 입력해주세요.",
+    RETRY_ERROR : "[ERROR] 대문자 R 또는 대문자 Q를 입력해주세요.",
+};
+
+module.exports = { Error_Message };
